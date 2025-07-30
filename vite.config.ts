2fdba52acf6b8ca4d918f4a58a050fda3eca5af6
skorbiz <skorbiz@gmail.com>
2025-07-30 11:26:41 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/' // Replace with your GitHub repo name
+  base: '/laursen/' // Replace with your GitHub repo name
 }));
