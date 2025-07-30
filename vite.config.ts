35ad2e66ed8e8b7227c154379473c16bdd57dc73
skorbiz <skorbiz@gmail.com>
2025-07-30 10:22:44 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -19,5 +18,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/skorbiz/' // Replace with your GitHub repo name
+  base: '/' // Since this will be the base directory
 }));
